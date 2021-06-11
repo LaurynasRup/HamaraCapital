6c5d9339018f89d813b0d516c6996fa29fad3e1c
Laurynas Rup <lauryfas@gmail.com>
2021-06-11 16:47:47 +0100

    Cors added

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-
+const cors = require('cors');
 const path = require('path');
+app.use(cors());
 
 //Redirect to https
 app.use((req, res, next) => {
